cbbf12dd335ffa5b3c581e6754fcbb76ded842d7
Mateusz Talma <talmamateusz@gmail.com>
2023-09-14 19:31:26 +0200

    Refactor collision detected during character move

--- DS0.5/Map.cpp
+++ DS0.5/Map.cpp
@@ -1,6 +1,43 @@
 #include "Map.h"
 #include "Helper.h"
 
+bool IsOccupiedDownSquare(const sf::Vector2f& posAfterMove)
+{
+	return static_cast<int>(posAfterMove.y) % static_cast<int>(size::cellSize) != 0;
+}
+
+bool IsOccupiedRightSquare(const sf::Vector2f& posAfterMove)
+{
+	return static_cast<int>(posAfterMove.x) % static_cast<int>(size::cellSize) != 0;
+}
+
+bool IsOccupiedDownAndRightSquare(const sf::Vector2f& posAfterMove)
+{
+	return static_cast<int>(posAfterMove.x) % static_cast<int>(size::cellSize) != 0 && static_cast<int>(posAfterMove.y) % static_cast<int>(size::cellSize) != 0;
+}
+
+std::vector<sf::Vector2f> prepareOccupiedPositionsByCharacter(const sf::Vector2f& posAfterMove, float x, float y)
+{
+	std::vector<sf::Vector2f> characterPositions;
+	characterPositions.push_back({ x, y });
+	if (IsOccupiedDownAndRightSquare(posAfterMove))
+	{
+		characterPositions.push_back({ x + 1, y });
+		characterPositions.push_back({ x, y + 1 });
+		characterPositions.push_back({ x + 1, y + 1 });
+	}
+	else if (IsOccupiedRightSquare(posAfterMove))
+	{
+		characterPositions.push_back({ x + 1, y });
+	}
+	else if (IsOccupiedDownSquare(posAfterMove))
+	{
+		characterPositions.push_back({ x, y + 1 });
+	}
+
+	return characterPositions;
+}
+
 void Map::Draw(sf::RenderWindow& window)
 {
 	for (int i = 0; i < m_map.size(); ++i)
@@ -17,28 +54,15 @@ bool Map::IsCollisionWithCharacter(const sf::Vector2f& posAfterMove, const std::
 	auto [xInt, yInt] = position::GetMapIndexesFromPosition({ posAfterMove.x, posAfterMove.y });
 	float x = static_cast<float>(xInt);
 	float y = static_cast<float>(yInt);
-	std::vector<sf::Vector2f> characterPositions;
-	characterPositions.push_back({ x, y });
-	if (static_cast<int>(posAfterMove.x) % static_cast<int>(size::cellSize) != 0 && static_cast<int>(posAfterMove.y) % static_cast<int>(size::cellSize) != 0)
-	{
-		characterPositions.push_back({ x + 1, y });
-		characterPositions.push_back({ x, y + 1});
-		characterPositions.push_back({ x + 1, y + 1});
-	}
-	else if (static_cast<int>(posAfterMove.x) % static_cast<int>(size::cellSize) != 0)
-	{
-		characterPositions.push_back({ x + 1, y });
-	}
-	else if (static_cast<int>(posAfterMove.y) % static_cast<int>(size::cellSize) != 0)
-	{
-		characterPositions.push_back({ x, y + 1 });
-	}
-	for (int i = 0; i < characterPositions.size(); i++)
-	{
-		CellState cellState = m_map[characterPositions[i].y][characterPositions[i].x].GetState();
+
+	std::vector<sf::Vector2f> characterPositions = prepareOccupiedPositionsByCharacter(posAfterMove, x, y);
+	
+	for (const auto[x, y] : characterPositions)
+	{ 
+		CellState cellState = m_map[y][x].GetState();
 		if (std::any_of(forbiddenStates.begin(), forbiddenStates.end(), [cellState](auto state) {return cellState == state; }))
 		{
-			return IsCollisionWithCell({characterPositions[i].x * size::cellSize, characterPositions[i].y * size::cellSize}, posAfterMove);
+			return IsCollisionWithCell({x * size::cellSize, y * size::cellSize}, posAfterMove);
 		}
 	}
 	return false;
